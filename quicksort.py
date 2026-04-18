8092e35cb66e11e6769fe88a391caff51cd821dd
ahmad-cod <ahmadadewale800@gmail.com>
2026-04-18 10:07:27 +0100

    Implement Quicksort Algorithm using inplace partition

--- quicksort.py
+++ quicksort.py
@@ -14,3 +14,32 @@ def quicksort(arr):
   # return quicksort(less) + equal + quicksort(greater)
 
 print(quicksort([10, 5, 10, 2, 2, 4, 8, 11, 5, 3]))
+
+# Quicksort using inplace partition
+def partition(arr, low, high):
+  pivot = arr[high]
+  i = low - 1
+  
+  for j in range(low, high):
+    if arr[j] <= pivot:
+      i += 1
+      arr[i], arr[j] = arr[j], arr[i]
+
+  arr[i + 1], arr[high] = arr[high], arr[i + 1]
+  return i + 1        # return pivot index
+
+def _quicksort_recursive(arr, low, high):
+  if low < high:
+    pivot_index = partition(arr, low, high)
+    _quicksort_recursive(arr, low, pivot_index - 1)
+    _quicksort_recursive(arr, pivot_index + 1, high)
+  # return arr        # Uncomment to return the arr directly from the quicksort algorithm
+
+def quicksort(arr):
+  return _quicksort_recursive(arr, 0, len(arr) - 1)
+
+print("Quicksort using inplace partition")
+nums = [10, 5, 10, 2, 2, 4, 8, 11, 5, 3]
+
+# print(quicksort(nums))
+print(nums)
